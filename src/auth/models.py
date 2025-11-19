b285e6ab435d7804c366ace6f23a4238ea4a2513
Zunaid Ali <zpokean@gmail.com>
2025-11-19 16:58:17 +0600

    user creation with password hashing

--- src/auth/models.py
+++ src/auth/models.py
@@ -20,6 +20,7 @@ class User(SQLModel, table = True):
     first_name: str
     last_name: str
     is_verified: bool = Field(default = False)
+    password_hash: str = Field(exclude = False)
     created_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default = datetime.now))
     updated_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default = datetime.now))
 
